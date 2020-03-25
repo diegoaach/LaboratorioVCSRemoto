f42bce37bff420a395727f1aba5432ab050c8a5e
diegoaach <61645376+diegoaach@users.noreply.github.com>
2020-03-25 18:57:17 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 18:49:36 2020
+
+@author: Diego
+"""
+
+a = int(input('Ingrese el primer numero: '))
+c = int(input('Ingrese el segundo numero: '))
+
+print('El producto es ',(a*c))
+print('El doble de a es',(a*2))

1776e621401f76e36a095fcd055b2ae860fffad5
diegoaach <61645376+diegoaach@users.noreply.github.com>
2020-03-26 18:25:47 -0500

    Filas de numeros progresivos

--- Preinforme_Lab9.py
+++ Preinforme_Lab9.py
@@ -5,13 +5,20 @@ Created on Thu Mar 26 17:24:41 2020
 @author: Diego
 """
 
+n = int(input('Ingrese el numero de filas que desea imprimir: '))
 
-n = int(input('Ingrese un numero de 4 cifras: '))
-n = abs(n)
-inverso = 0
+contn = 0
+fila = 0
+nf = 1
+while contn<n:
+    
+    fila = fila*10 + nf
+    nf += 1
+    contn += 1
+    print(fila)
+    
+    if nf>9:
+        nf = 0
 
-while n>0:
-    inverso = (inverso*10) + (n%10)
-    n = n//10
+print('Filas completadas: ',contn)
 
-print (inverso)

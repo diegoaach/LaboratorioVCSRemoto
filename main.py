606c6b20bcd2d92ab2e8ba860f374a9d85363578
diegoaach <61645376+diegoaach@users.noreply.github.com>
2020-03-25 19:39:06 -0500

    Mi primera modificacion

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ Created on Wed Mar 25 18:49:36 2020
 @author: Diego
 """
 
-a = int(input('Ingrese el primer numero: '))
-c = int(input('Ingrese el segundo numero: '))
+a = int(input('Ingrese el numero a: '))
+b = int(input('Ingrese el numero b: '))
+c = int(input('Ingrese el numero c: '))
+d = int(input('Ingrese el numero d: '))
 
-print('El producto es ',(a*c))
-print('El doble de a es',(a*2))
+print('El producto de a por c es ',(a*c))
+print('El doble de a es ',(a*2))
+print('El cuadrado de b ',(b**2))
+print('La raiz cuadrada de d es ',(d**(1/2)))
